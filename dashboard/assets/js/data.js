e0c28739074e355be8d0a7c1dc1ccfc0a2da0333
meshnesh <tonnie.nesh@gmail.com>
2016-12-19 16:29:27 +0300

    edited content on json data and from the dashboard

--- dashboard/assets/js/data.js
+++ dashboard/assets/js/data.js
@@ -10,10 +10,10 @@ $.ajax({
             for (var i=0; i<data.attractions.length;i++){
                 text += '<tr>' +
                             '<td>' + data.attractions[i].attraction_site_id + '</td>' +
+                            '<td>' + data.attractions[i].categories + '</td>' +
                             '<td>' + data.attractions[i].site_name + '</td>' +
                             '<td>' + data.attractions[i].opening_hrs + '</td>' +
                             '<td>' + data.attractions[i].location_name + '</td>' +
-                            '<td><a href="#">' + data.attractions[i].site_name + '</a></td>' +
                         '</tr>';
                 // end of the function
 
@@ -33,7 +33,7 @@ $.ajax({
 
         var text = '';
         var x = data.accommodations.length;
-            for (var x=0; x<=10;x++){
+            for (var x=0; x<=7;x++){
                 text += '<tr>' +
                             '<td>' + data.accommodations[x].location_name + '</td>' +
                             '<td>' + data.accommodations[x].restaurant_name + '</td>' +
