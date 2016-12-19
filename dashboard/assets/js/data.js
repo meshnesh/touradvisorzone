f6f16a48f99025ae7f44702ccf6920d97ffcefe3
meshnesh <tonnie.nesh@gmail.com>
2016-12-19 14:29:15 +0300

    picking data from the url

--- dashboard/assets/js/data.js
+++ dashboard/assets/js/data.js
@@ -7,47 +7,19 @@ $.ajax({
 
 
         var text = '';
-            for (var i=0;i<data.partnerQoutes.length;i++){
-                text += '<div class="row">' +
-                            '<div id="logname" class="large-3 columns">' +
-                                '<div class="row">' +
-                                    '<div class="small-3 columns logos"><img class="logo" data.partnerQoutes[i].partnerLogo src=" http://design.ubuntu.com/wp-content/uploads/ubuntu-logo32.png"></div>' +
-                                    '<div class="small-9 columns names">' + data.partnerQoutes[i].partnerName + '</div>' +
-                                '</div>' +
-                            '</div>' + //logo and name
-
-                            '<div class="large-6 columns">' +
-                                '<ul>' +
-                                    '<li>' + data.partnerQoutes[i].partnerBenefits +  '</li>'+
-                                '</ul>'+
-                            '</div>' +
-
-                            '<div class="large-3 columns">' +
-                                '<div class="large-6 columns"><p class="cost">'  + data.partnerQoutes[i].partnerCost +'</p></div>' +
-                                '<div class="large-6 columns"><fieldset><input type="radio" type="commit " class="butt" name="partnerID" value="'+data.partnerQoutes[i].partnerID+'" id="'+data.partnerQoutes[i].partnerID+'"></fieldset></div>' +
-                            '</div>' +
-                        '</div>';
-
-                $(document).on('click', '.butt', function (e) {
-                    console.log(e);
-                });
-
+            for (var i=0;i<data.attractions.length;i++){
+                text += '<tr>' +
+                            '<td>' + data.attractions[i].attraction_site_id + '</td>' +
+                            '<td>' + data.attractions[i].site_name + '</td>' +
+                            '<td>' + data.attractions[i].opening_hrs + '</td>' +
+                            '<td>' + data.attractions[i].location_name + '</td>' +
+                            '<td>' + data.attractions[i].site_name + '</td>' +
+                        '</tr>';
                 // end of the function
 
             }
-            $( '#card' ).html( text );
+            $( '#table' ).html( text );
     
     }
 });
-
-
-
-// $.ajax('http://touradvisorzone.com/touradvisorphp/all_attractions.php', {
-//                 type: 'GET'
-//                 , dataType: 'json'
-//                 , success: function (result) {
-//                     console.log(result);
-
-//                 }
-//             });
 
