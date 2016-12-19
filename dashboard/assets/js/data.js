e3f6bc07a4b5ddc733f8b7cf7014d47de63c74c6
meshnesh <tonnie.nesh@gmail.com>
2016-12-19 15:56:20 +0300

    passed accomodation data to the dashboard

--- dashboard/assets/js/data.js
+++ dashboard/assets/js/data.js
@@ -7,13 +7,13 @@ $.ajax({
 
 
         var text = '';
-            for (var i=0;i<data.attractions.length;i++){
+            for (var i=0; i<data.attractions.length;i++){
                 text += '<tr>' +
                             '<td>' + data.attractions[i].attraction_site_id + '</td>' +
                             '<td>' + data.attractions[i].site_name + '</td>' +
                             '<td>' + data.attractions[i].opening_hrs + '</td>' +
                             '<td>' + data.attractions[i].location_name + '</td>' +
-                            '<td>' + data.attractions[i].site_name + '</td>' +
+                            '<td><a href="#">' + data.attractions[i].site_name + '</a></td>' +
                         '</tr>';
                 // end of the function
 
@@ -22,4 +22,31 @@ $.ajax({
     
     }
 });
+
+$.ajax({
+    type: "GET"
+    , url: 'http://touradvisorzone.com/touradvisorphp/accommodation_map.php'
+    , dataType: "json", // serializes the form's elements.
+    success: function (data) {
+        console.log(data); // show response from the php script.
+
+
+        var text = '';
+        var x = data.accommodations.length;
+            for (var x=0; x<=10;x++){
+                text += '<tr>' +
+                            '<td>' + data.accommodations[x].location_name + '</td>' +
+                            '<td>' + data.accommodations[x].restaurant_name + '</td>' +
+                            '<td>' + '<button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs"> <i class="fa fa-edit"></i> </button>'+ 
+                            '<button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs"> <i class="fa fa-times"></i> </button>'+
+                            '</td>' +
+                        '</tr>';
+                // end of the function
+
+            }
+            $( '#accomodation' ).html( text );
+    
+    }
+});
+
 
